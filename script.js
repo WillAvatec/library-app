08f021b3f359658318a5c270ad77c4b9569a60f3
Will <avatec2312@gmail.com>
2022-11-17 03:01:57 -0500

    Creating function that render book on table

---
+++ script.js
@@ -0,0 +1,48 @@
+let library = [
+    {
+        bName:"Hobbit", bAuthor:"Tolkien", bPages: "600", read: true
+    }
+    ,{
+        bName:"MahBook", bAuthor:"Me", bPages: "100", read:false
+    }
+]
+
+// Referencia a la tabla 
+const table = document.querySelector(".styled-table tbody");
+
+// Funcion que se encarga de asignar los datos de la forma en td's
+// y tambien se encarga de los appendChild hacia la fila
+
+function setBookValues(row, name, author, pages, isRead) {
+    const rowFixed = row
+    const bName = document.createElement("td");
+    const bAuthor = document.createElement("td");
+    const bPages = document.createElement("td");
+    const read = document.createElement("td");
+    const erase = document.createElement("td");
+
+    bName.textContent = name;
+    bAuthor.textContent = author;
+    bPages.textContent = pages;
+    isRead===true ? read.textContent = "SI": read.textContent = "NO";
+    erase.innerHTML = "&times;"
+
+    rowFixed.appendChild(bName);
+    rowFixed.appendChild(bAuthor);
+    rowFixed.appendChild(bPages);
+    rowFixed.appendChild(read);
+    rowFixed.appendChild(erase);
+
+    return rowFixed;
+}
+
+
+function addBook(){
+    const tr = document.createElement("tr");
+    
+    table.appendChild(tr);
+}
+
+function renderLibrary(){
+    addBook();
+}
