22e05a51d00151f4afc02d5ec35046f7fdb49798
Will <avatec2312@gmail.com>
2022-11-18 16:58:40 -0500

    Adding modal script

--- script.js
+++ script.js
@@ -39,10 +39,35 @@ function setBookValues(row, name, author, pages, isRead) {
 
 function addBook(){
     const tr = document.createElement("tr");
-    
+    setBookValues(tr,"mi libro", "yo", "20 pages", true);
     table.appendChild(tr);
 }
 
 function renderLibrary(){
     addBook();
 }
+
+// Modal javascript
+
+
+// Referencias a los  elementos del DOM
+
+const openBtn = document.querySelector("#open");
+const modal = document.querySelector(".modal");
+const span = document.getElementsByClassName("close")[0];
+
+// Botones del Modal
+
+openBtn.addEventListener("click", ()=>{
+    modal.style.display = "block"
+});
+
+span.addEventListener("click", ()=>{
+    modal.style.display = "none"
+})
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+})
